62f1d426821fc27fa47e386601a0044009fa9683
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-08 10:06:20 -0800

    Reduce public interface

--- index.mjs
+++ index.mjs
@@ -1,4 +1,1 @@
 export { default as Storehouse } from './data/data-store.js'
-export { default as DataBlock } from './data/data-block.js'
-export { default as EnumDataBlock } from './data/enum-data-block.js'
-export { default as ArrayDataBlock } from './data/array-data-block.js'
