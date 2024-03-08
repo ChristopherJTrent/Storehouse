de6c00540baf1504cb6e55cb7a244624a64dd02c
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-08 10:23:18 -0800

    document API

--- data/data-store.js
+++ data/data-store.js
@@ -13,6 +13,13 @@ export default class Storehouse {
     /** @type {Map<String, DataBlock>} */
     static storage = new Map()
 
+    /**
+     * @template T
+     * Registers a provider that will track the changes to a single value
+     * @param {String} key the name of the provider
+     * @param {T} [startingValue=0] the starting value of the provider
+     * @returns {[(value: T)=>undefined, ()=>T]} a pair of functions [set, get]
+     */
     static registerProvider(key, startingValue = 0) {
         Storehouse.storage.set(key, new DataBlock(startingValue))
         return [
@@ -22,7 +29,13 @@ export default class Storehouse {
             () => Storehouse.storage.get(key)
         ]
     }
-
+    /**
+     * @template T
+     * Registers a provider that will track the changes to an array of values
+     * @param {String} key the name of the provider
+     * @param {T[]} [startingValue=[]] the starting value of the provider
+     * @returns {[(index:Number, value:T)=>undefined, ()=>T[]]} a pair of functions [set, get]
+     */
     static registerArrayProvider(key, startingValue = []) {
         Storehouse.storage.set(key, new ArrayDataBlock(startingValue));
         return [
@@ -32,6 +45,14 @@ export default class Storehouse {
             () => Storehouse.storage.get(key).value
         ]
     }
+    /**
+     * @template T
+     * 
+     * @param {String} key the name of the provider
+     * @param {T} allowedValues the permitted range of starting values
+     * @param {T} [startingValue=allowedValues[0]] the starting value for the provider, which must be a member of allowedValues
+     * @returns {[(value: T)=>undefined, ()=>T]} a pair of functions [set, get]
+     */
     static registerEnumProvider(key, allowedValues, startingValue = allowedValues[0]) {
         Storehouse.storage.set(key, new EnumDataBlock(allowedValues, startingValue))
         return [
@@ -39,16 +60,37 @@ export default class Storehouse {
             () => Storehouse.storage.get(key).value
         ]
     }
+    /**
+     * registers a subscriber to a given provider
+     * @template T
+     * @param {String} key the name of the provider
+     * @param {(value: T) => undefined} callback 
+     */
     static registerSubscriber(key, callback) {
-        return Storehouse.storage.get(key).subscribe(callback);
+        Storehouse.storage.get(key).subscribe(callback);
     }
+    /**
+     * registers a subscriber to a given aggregate provider
+     * @template T
+     * @param {String} key the name of the provider
+     * @param {(value: T) => undefined} callback a function that will handle the value 
+     * @param {(list: T[]) => T} aggregator a function that aggregates an array of values into a single value
+     */
     static registerAggregateSubscriber(key, callback, aggregator = sum) {
-        const block = Storehouse.storage.get(key)
-        return block.subscribeAggregate(callback, aggregator);
-    }
+        Storehouse.storage.get(key).subscribeAggregate(callback, aggregator);
+    } 
+    /**
+     * forces an update to a provider's subscribers
+     * @param {String} key the name of the provider
+     */
     static forceUpdate(key) {
         Storehouse.storage.get(key)?.alertSubscribers();
     }
+    /**
+     * checks if a given provider name is registered
+     * @param {String} key the name of the provider to verify
+     * @returns {boolean} true if present
+     */
     static hasProvider(key) {
         return Storehouse.storage.has(key);
     }
